a7bca45a5535f903888918aa39fbdb43910d3eb5
samuelpanzera <samuelpanzera@gmail.com>
2022-01-05 16:05:37 -0300

    versão 1.0

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 var currentNumberSpan = document.getElementById('currentNumber');
 var currentNumber = 0;
-
+var galo;
 
 function increment() {
   currentNumber += 1;
   currentNumberSpan.innerHTML = currentNumber;
   document.title = currentNumber;
+    if(currentNumber == 13){
+
+      galo = '13 é galo';
+      document.title = galo; //muda o titulo da aba document.title, aceita diversos valores
+     
+      document.getElementById('galoDoido').setAttribute('src', 'img/Escudo_atletico_mineiro_de_tete.svg'); //faltou chamar função
+          
+      document.getElementById("galo").textContent="13 é galo";
+      
+    }else{
+      document.getElementById("galo").textContent="";
+      document.getElementById('galoDoido').setAttribute('src', '');
+    }
 
 }
 
@@ -13,5 +26,16 @@ function decrement() {
   currentNumber -= 1;
   currentNumberSpan.innerHTML = currentNumber;
   document.title = currentNumber;
+    if(currentNumber == 13){
+      galo = '13 é galo';
+      document.title = galo;
+      document.getElementById('galoDoido').setAttribute('src', 'img/Escudo_atletico_mineiro_de_tete.svg');
+      
+      document.getElementById("galo").textContent="13 é galo";
+      
+    }else{
+      document.getElementById("galo").textContent="";
+      document.getElementById('galoDoido').setAttribute('src', '');
+    }
 }
 
